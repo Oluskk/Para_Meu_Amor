24300a95069511e04d9c3e6e8ead6e6fdd8dcf0f
Lucas <att.lucassilva@gmail.com>
2025-04-08 12:38:38 -0300

    para o meu amor

---
+++ PraVoce/src/br/com/Lsousa/ParaOAmorDaMinhaVIDA.java
@@ -0,0 +1,11 @@
+package br.com.Lsousa;
+
+public class ParaOAmorDaMinhaVIDA {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        while (contador < 10) {
+            System.out.println("Te amo, amor! ❤️ (" + contador + ")");
+        }
+    }
+}
